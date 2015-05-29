ba90d867272bb85d81aa9bbdcd67aa7c98b0354c
Andrew Kuhnhausen <andrew.kuhnhausen@lookout.com>
2015-05-29 09:22:48 -0700

    Add failure and version to health check
    This health check previously only returned 200 and never gave errors in
    the output. For posterity, we'll add the version info, too.
    
    Update tests to check both failure and success

--- src/health_check.lua
+++ src/health_check.lua
@@ -2,54 +2,72 @@
 -- This script serves up an HTML page that displays current health of the
 -- BorderPatrol. The only check, currently, is that memcache is reachable.
 --
-local errors = {}
 local health_check = {}
 
 -- print out the actual HTML
 function health_check.output(errors)
-  ngx.header.content_type = 'text/html';
-  ngx.print([[
+  local output = [[
   <html>
     <head>
       <title>Border Patrol Health</title>
     </head>
     <body>
-  ]])
+  ]]
+
+  output = output .. "<h3>Version " .. bp_version .. "</h3>"
 
   if #errors > 0 then
-    ngx.print("<h3>Errors</h3><ul>")
+    output = output .. "<h3>Errors</h3><ul>"
     for i, v in ipairs(errors) do
-      ngx.print("<li>" .. v .. "</li>")
+      output = output .. "<li>" .. v .. "</li>"
     end
-    ngx.print("</ul>")
+    output = output .. "</ul>"
   else
-    ngx.print("Everything is ok.")
+    output = output .. "Everything is ok."
   end
 
-  ngx.print([[
+  output = output .. [[
     </body>
   </html>
-  ]])
+  ]]
+
+  return output
 end
 
-local res = ngx.location.capture('/session?id=health_check', { method = ngx.HTTP_POST, body = os.time() })
-if not res.status == ngx.HTTP_OK then
-  errors[#errors+1] = "memcache add: " .. res.status .. ": " .. res.body
+local function get_errors()
+  local errors = {}
+  local res = ngx.location.capture('/session?id=health_check', { method = ngx.HTTP_POST, body = os.time() })
+  if not (res.status == ngx.HTTP_CREATED) then
+    errors[#errors+1] = "memcache add: " .. res.status .. ": " .. res.body
+  end
+
+  res = ngx.location.capture('/session?id=health_check')
+  if not (res.status == ngx.HTTP_OK) then
+    errors[#errors+1] = "memcache get: " .. res.status .. ": " .. res.body
+  end
+
+  res = ngx.location.capture('/session?id=health_check', { method = ngx.HTTP_PUT, body = os.time() })
+  if not (res.status == ngx.HTTP_CREATED) then
+    errors[#errors+1] = "memcache set: " .. res.status .. ": " .. res.body
+  end
+
+  res = ngx.location.capture('/session?id=health_check', { method = ngx.HTTP_DELETE })
+  if not (res.status == ngx.HTTP_OK) then
+    errors[#errors+1] = "memcache delete: " .. res.status .. ": " .. res.body
+  end
+
+  return errors
 end
 
-res = ngx.location.capture('/session?id=health_check')
-if not res.status == ngx.HTTP_OK then
-  errors[#errors+1] = "memcache get: " .. res.status .. ": " .. res.body
+
+local errors = get_errors()
+local output = health_check.output(errors)
+
+if (#errors > 0) then
+  ngx.status = ngx.HTTP_INTERNAL_SERVER_ERROR
+else
+  ngx.status = ngx.HTTP_OK
 end
 
-res = ngx.location.capture('/session?id=health_check', { method = ngx.HTTP_PUT, body = os.time() })
-if not res.status == ngx.HTTP_OK then
-  errors[#errors+1] = "memcache set: " .. res.status .. ": " .. res.body
-end
-
-res = ngx.location.capture('/session?id=health_check', { method = ngx.HTTP_DELETE })
-if not res.status == ngx.HTTP_OK then
-  errors[#errors+1] = "memcache delete: " .. res.status .. ": " .. res.body
-end
-
-health_check.output(errors)
+ngx.header.content_type = 'text/html'
+ngx.print(output)
